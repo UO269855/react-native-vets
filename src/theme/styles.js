826db43f6f0f16fe1b637efff31aa248dec5ae47
aleex <uo269855@uniovi.es>
2025-02-23 17:38:40 +0100

    17, 18 y 19

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -1,10 +1,12 @@
-import { ActivityIndicator, Button } from "react-native-paper";
+import { ActivityIndicator, Avatar, Button, List } from "react-native-paper";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { colors } from "./colors";
 import { sizes } from "./sizes";
 import { TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
+import MapView from "react-native-maps";
+import { CameraView } from "expo-camera";
 
 export const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
@@ -91,4 +93,35 @@ export const ContactButton = styled(Button).attrs({
 
 export const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+export const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+
+export const AvatarContainer = styled.View`
+  align-items: center;
+  padding: ${(props) => props.theme.space[3]};
+`;
+export const AvatarIcon = styled(Avatar.Icon).attrs({
+  size: 180,
+  backgroundColor: colors.button,
+})``;
+export const AvatarImage = styled(Avatar.Image).attrs({
+  size: 180,
+})``;
+export const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const CameraViewer = styled(CameraView)`
+  flex: 1;
+`;
+export const CameraContainer = styled(TouchableOpacity)`
+  flex: 1;
+`;
+export const NoFavouritesYet = styled.View`
+  align-items: center;
+  justify-content: center;
 `;
